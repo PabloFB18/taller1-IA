166183cc05100b93370015e56ecef7cf962d6e6d
PabloFB18 <pfb006@alumnos.ucn.cl>
2017-11-07 17:55:30 -0300

    interfaz no grafica lista, ya no falta lo del commit anterior

--- Pablo/alphaBeta.py
+++ Pablo/alphaBeta.py
@@ -23,7 +23,7 @@ def jugar(estado_actual):
     :param estado_actual: Es el estado actual
     :return: Jugada a realizar
     """
-    jugadas = [[]]
+    jugadas = [[0, 0, 0]]
     alfa = -99
     beta = 99
     maximo(estado_actual, alfa, beta, jugadas)
